0fc86980319ef829df160819c837fe798a234f2c
Philip Neustrom <philipn@gmail.com>
2013-12-11 21:11:42 -0800

    +v0.1.1

--- rest_framework_chain/__init__.py
+++ rest_framework_chain/__init__.py
@@ -1,5 +1,5 @@
 from .filters import RelatedFilter, AllLookupsFilter
 from .filterset import ChainedFilterSet
 
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 __version__ = VERSION  # alias
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def get_package_data(package):
 
 setup(
     name='djangorestframework-chain',
-    version='0.1.0',
+    version='0.1.1',
     url='http://github.com/philipn/django-rest-framework-chain',
     license='MIT',
     description='Chained relations and filtering for Django REST Framework',
