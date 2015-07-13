652f8feb5b1e11d4c3304adcfc49ff53517d1e5a
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2015-07-13 16:27:40 -0300

    Fix ArrayDecimalField clean method
    value could be None when the clean method gets called, if so then we
    should return None

--- rest_framework_filters/fields.py
+++ rest_framework_filters/fields.py
@@ -2,6 +2,9 @@ from django import forms
 
 class ArrayDecimalField(forms.DecimalField):
     def clean(self, value):
+        if value is None:
+            return None
+
         out = []
         for val in value.split(','):
             out.append(super(ArrayDecimalField, self).clean(val))
