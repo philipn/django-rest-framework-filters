75bfa1f3b1399e7b85a5dcbb02395b1f2304eea5
Ryan P Kilby <rpkilby@ncsu.edu>
2016-04-04 00:09:29 -0400

    Fix #79, add transform recursion prevention

--- rest_framework_filters/utils.py
+++ rest_framework_filters/utils.py
@@ -2,6 +2,7 @@
 from collections import OrderedDict
 
 from django.db.models.constants import LOOKUP_SEP
+from django.db.models.expressions import Expression
 from django.db.models.lookups import Transform
 from django.utils import six
 
@@ -14,10 +15,46 @@ def lookups_for_field(model_field):
 
     for expr, lookup in six.iteritems(class_lookups(model_field)):
         if issubclass(lookup, Transform):
+            transform = lookup(Expression(model_field))
             lookups += [
-                LOOKUP_SEP.join([expr, transform]) for transform
-                in lookups_for_field(lookup(model_field).output_field)
+                LOOKUP_SEP.join([expr, sub_expr]) for sub_expr
+                in lookups_for_transform(transform)
             ]
+
+        else:
+            lookups.append(expr)
+
+    return lookups
+
+
+def lookups_for_transform(transform):
+    """
+    Generates a list of subsequent lookup expressions for a transform.
+
+    Note:
+    Infinite transform recursion is only prevented when the subsequent and
+    passed in transforms are the same class. For example, the ``Unaccent``
+    transform from ``django.contrib.postgres``.
+    There is no cycle detection across multiple transforms. For example,
+    ``a__b__a__b`` would continue to recurse. However, this is not currently
+    a problem (no builtin transforms exhibit this behavior).
+
+    """
+    lookups = []
+
+    for expr, lookup in six.iteritems(class_lookups(transform.output_field)):
+        if issubclass(lookup, Transform):
+
+            # type match indicates recursion.
+            if type(transform) == lookup:
+                continue
+
+            sub_transform = lookup(transform)
+            lookups += [
+                LOOKUP_SEP.join([expr, sub_expr]) for sub_expr
+                in lookups_for_transform(sub_transform)
+            ]
+
         else:
             lookups.append(expr)
 
@@ -28,12 +65,12 @@ def class_lookups(model_field):
     """
     Get a compiled set of class_lookups for a model field.
     """
-    field_class = model_field.__class__
+    field_class = type(model_field)
     class_lookups = OrderedDict()
 
     # traverse MRO in reverse, as this puts standard
     # lookups before subclass transforms/lookups
-    for cls in field_class.mro()[::-1]:
+    for cls in reversed(field_class.mro()):
         if hasattr(cls, 'class_lookups'):
             class_lookups.update(getattr(cls, 'class_lookups'))
 
