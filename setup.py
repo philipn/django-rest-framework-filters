39baeb28d2687cdf739345f8bcc4e1286bfc9f49
Philip Neustrom <philipn@gmail.com>
2016-11-19 16:45:59 -0500

    v0.9.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def get_package_data(package):
 
 setup(
     name='djangorestframework-filters',
-    version='0.9.0',
+    version='0.9.1',
     url='http://github.com/philipn/django-rest-framework-filters',
     license='MIT',
     description='Better filtering for Django REST Framework',
