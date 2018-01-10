adccc4aea591ff60816d037973e5ebda5fb4d81f
Ryan P Kilby <rpkilby@ncsu.edu>
2018-01-10 16:10:42 -0500

    Add manifest, use builtin setuptools fn

--- setup.py
+++ setup.py
@@ -1,37 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
-import os
+from setuptools import setup, find_packages
 
 
 with open('README.rst') as f:
     README = f.read()
-
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
 
 
 setup(
@@ -43,8 +17,8 @@ setup(
     description='Better filtering for Django REST Framework',
     author='Philip Neustrom',
     author_email='philipn@gmail.com',
-    packages=get_packages('rest_framework_filters'),
-    package_data=get_package_data('rest_framework_filters'),
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
     zip_safe=False,
     install_requires=[
         'djangorestframework',
